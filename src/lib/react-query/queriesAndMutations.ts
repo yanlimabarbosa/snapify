29b9c6df81a3bc99cda95df32daa4797270d467f
Yan Lima Barbosa <yanbr763@gmail.com>
2023-12-22 05:40:32 -0300

    feat(auth): add sign-out functionality
    - Implement the signOutAccount function to delete the current user's session.
    - This allows users to sign out and end their authenticated session.

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,7 +4,7 @@ import {
   useQueryClient,
   useInfiniteQuery,
 } from "@tanstack/react-query"
-import { createUserAccount, signInAccount } from "../appwrite/api"
+import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api"
 import { INewUser } from "@/types"
 
 export const useCreateUserAccount = () => {
@@ -23,3 +23,8 @@ export const useSignInAccount = () => {
   })
 }
 
+export const useSignOutAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount
+  })
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,35 @@
-import React from "react";
+export type INavLink = {
+  imgURL: string;
+  route: string;
+  label: string;
+};
+
+export type IUpdateUser = {
+  userId: string;
+  name: string;
+  bio: string;
+  imageId: string;
+  imageUrl: URL | string;
+  file: File[];
+};
+
+export type INewPost = {
+  userId: string;
+  caption: string;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
+
+export type IUpdatePost = {
+  postId: string;
+  caption: string;
+  imageId: string;
+  imageUrl: URL;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
 
 export type IUser = {
   id: string;
@@ -14,13 +45,4 @@ export type INewUser = {
   email: string;
   username: string;
   password: string;
-}
-
-export type IContextType = {
-  user: IUser;
-  isLoading: boolean;
-  setUser: React.Dispatch<React.SetStateAction<IUser>>;
-  isAuthenticated: boolean;
-  setAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
-  checkAuthUSer: () => Promise<Boolean>;
 };
