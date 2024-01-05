ef96b2845436acf3aa3f874d7654d03132bd1f1e
Yan Lima Barbosa <yanbr763@gmail.com>
2024-01-05 15:40:23 -0300

    minor

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,16 +10,6 @@ export const sidebarLinks = [
     label: "Explore",
   },
   {
-    imgURL: "/assets/icons/people.svg",
-    route: "/all-users",
-    label: "People",
-  },
-  {
-    imgURL: "/assets/icons/bookmark.svg",
-    route: "/saved",
-    label: "Saved",
-  },
-  {
     imgURL: "/assets/icons/gallery-add.svg",
     route: "/create-post",
     label: "Create Post",
@@ -36,11 +26,6 @@ export const bottombarLinks = [
     imgURL: "/assets/icons/wallpaper.svg",
     route: "/explore",
     label: "Explore",
-  },
-  {
-    imgURL: "/assets/icons/bookmark.svg",
-    route: "/saved",
-    label: "Saved",
   },
   {
     imgURL: "/assets/icons/gallery-add.svg",
