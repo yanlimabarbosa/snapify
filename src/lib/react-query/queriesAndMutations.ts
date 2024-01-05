e68c6b05f421b10302514206e7feb95f08504dd3
Yan Lima Barbosa <yanbr763@gmail.com>
2024-01-05 15:35:01 -0300

    feat(queries): implement useGetPosts and useSearchPosts hooks for post retrieval
    - Added useGetPosts hook for fetching an infinite list of posts with pagination.
    - Utilizes useInfiniteQuery with getInfinitePosts function.
    - Added useSearchPosts hook for searching posts based on a given searchTerm.
    - Utilizes useQuery with searchPosts function, enabling/disabling based on the presence of searchTerm.
    
    These hooks enhance the application with efficient post retrieval and search capabilities.

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,7 +4,7 @@ import {
   useQueryClient,
   useInfiniteQuery,
 } from "@tanstack/react-query"
-import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getPostById, getRecentPosts, likePost, savePost, signInAccount, signOutAccount, updatePost } from "../appwrite/api"
+import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getInfinitePosts, getPostById, getRecentPosts, likePost, savePost, searchPosts, signInAccount, signOutAccount, updatePost } from "../appwrite/api"
 import { INewPost, INewUser, IUpdatePost } from "@/types"
 import { QUERY_KEYS } from "./queryKeys"
 
@@ -67,7 +67,7 @@ export const useSavePost = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: ({ postId, userId }: { postId: string, userId: string[] }) => savePost(userId, postId),
+    mutationFn: ({ postId, userId }: { postId: string, userId: string }) => savePost(userId, postId),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_RECENT_POSTS] })
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_POSTS] })
@@ -96,7 +96,7 @@ export const useGetCurrentUser = () => {
   })
 }
 
-export const useGetPostByID = (postId: string) => {
+export const useGetPostById = (postId: string) => {
   return useQuery({
     queryKey: [QUERY_KEYS.GET_POST_BY_ID, postId],
     queryFn: () => getPostById(postId),
@@ -121,12 +121,34 @@ export const useDeletePost = () => {
   const QueryClient = useQueryClient()
 
   return useMutation({
-    mutationFn: ({ postId, imageId }: { postId: string, imageId: string }) => deletePost(postId, imageId),
+    mutationFn: ({ postId, imageId }: { postId?: string, imageId?: string | undefined }) => deletePost(postId, imageId),
     onSuccess: () => {
       QueryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
       })
     }
+  })
+}
+
+export const useGetPosts = () => {
+  return useInfiniteQuery({
+    queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+    queryFn: getInfinitePosts,
+    getNextPageParam: (lastPage) => {
+      if (lastPage && lastPage.documents.length === 0) return null
+
+      const lastId = lastPage.documents[lastPage?.documents.length - 1].$id
+
+      return lastId
+    }
+  })
+}
+
+export const useSearchPosts = (searchTerm: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.SEARCH_POSTS, searchTerm],
+    queryFn: () => searchPosts(searchTerm),
+    enabled: !!searchTerm
   })
 }
 
