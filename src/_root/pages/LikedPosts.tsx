097e93a5a74df95ead378387769c2dda5ac29586
Yan Lima Barbosa <yanbr763@gmail.com>
2024-01-14 04:48:41 -0300

    feat: add LikedPosts component to display user's liked posts

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,4 +1,26 @@
-const LikedPosts = () => {
-  return <div>LikedPosts</div>
+import GridPostList from "@/components/shared/GridPostList"
+import Loader from "@/components/ui/Loader"
+import { Models } from "appwrite"
+
+type LikedPostProps = {
+  posts: Models.Document[]
 }
+
+const LikedPosts = ({ posts }: LikedPostProps) => {
+  if (!posts)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    )
+
+  return (
+    <>
+      {posts.length === 0 && <p className="text-light-4">No liked posts</p>}
+
+      <GridPostList posts={posts} showStats={false} />
+    </>
+  )
+}
+
 export default LikedPosts
